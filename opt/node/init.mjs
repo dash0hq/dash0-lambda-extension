0cb8b59aac84ee9c2f29d8ce2cebb48c5a3726d8
Moshe Shaham <moshe.shaham@dash0.com>
2026-01-22 14:24:04 +0200

    feat: enable /v1/logs endpoint

--- opt/node/init.mjs
+++ opt/node/init.mjs
@@ -1,24 +1,13 @@
-console.log("init.mjs line 1");
-
 import * as lumigo from "@lumigo/opentelemetry";
-console.log("init.mjs line 4");
-
 import { AwsLambdaInstrumentation } from '@opentelemetry/instrumentation-aws-lambda';
-console.log("init.mjs line 7");
-
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-console.log("init.mjs line 10");
-
 import {register} from "module";
-console.log("init.mjs line 13");
 
 
 console.log("Lumigo OpenTelemetry initialized in Node.js Lambda function.....");
 
 const awsLambdaInstrumentation = new AwsLambdaInstrumentation({});
-console.log("init.mjs line 19");
 const tracerProvider = (await lumigo.init).tracerProvider;
-console.log("init.mjs line 21");
 
 registerInstrumentations({
     instrumentations: [
@@ -26,7 +15,5 @@ registerInstrumentations({
     ],
     tracerProvider
 });
-console.log("init.mjs line 29");
 
 register('import-in-the-middle/hook.mjs', import.meta.url);
-console.log("init.mjs line 32");
