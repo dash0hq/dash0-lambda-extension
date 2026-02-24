a6afe750a2f074509b29d446f7e1a9eaaa9b2dc4
Moshe Shaham <moshe.shaham@dash0.com>
2026-02-24 12:35:54 +0200

    clo-249-setup-production-deployment-for-the-lambda-extension

--- integration-tests/tests/src/config.ts
+++ integration-tests/tests/src/config.ts
@@ -1,4 +1,4 @@
 export const DASH0_ENDPOINT = 'https://api.eu-west-1.aws.dash0-dev.com/api/';
-export const DASH0_TOKEN = 'auth_oEiAAAy5hZvVsEAADPm4uDyV7OcBmU4B';
+export const DASH0_TOKEN = process.env.DASH0_DEV_API_TOKEN!;
 export const MAX_ATTEMPTS = 10;
 export const RETRY_DELAY_MS = 5_000;
