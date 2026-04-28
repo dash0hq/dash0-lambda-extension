4b5a92ae4562b1014eba54ece93f69e427aa3d85
Moshe Shaham <moshe.shaham@dash0.com>
2026-04-28 12:41:59 +0300

    clo-525-fix-vulnerability-in-rust-rustls-webpki

--- integration-tests/tests/src/config.ts
+++ integration-tests/tests/src/config.ts
@@ -1,6 +1,6 @@
 export const DASH0_ENDPOINT = 'https://api.eu-west-1.aws.dash0-dev.com/api/';
 export const DASH0_LAMBDA_TESTS_DATASET = 'lambda-extension-tests';
 export const DASH0_TOKEN = process.env.DASH0_DEV_API_TOKEN!;
-export const MAX_ATTEMPTS = 20;
-export const RETRY_DELAY_MS = 7_000;
+export const MAX_ATTEMPTS = 50;
+export const RETRY_DELAY_MS = 10_000;
 export const TEST_TIMEOUT_MS = 6 * 60_000; // 3 minutes
