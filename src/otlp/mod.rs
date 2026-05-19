10910b9cb8ec1ca7f1eebd3d078c4da636c3491c
Moshe Shaham <moshe.shaham@dash0.com>
2026-05-19 08:15:07 +0300

    clo-634-fix-lambda-metric-resource-attributes

--- src/otlp/mod.rs
+++ src/otlp/mod.rs
@@ -8,6 +8,7 @@ pub mod masking;
 pub mod metrics_creation;
 pub mod metrics_receiver;
 pub mod receiver;
+pub mod resources;
 pub mod span_creation;
 pub mod span_link_extractor;
 pub mod span_mutations;
